4878065ae71e0ac706e83873416cdb6b519e8cbf
malorcmario <malorcmario@gmail.com>
2024-02-16 18:26:15 -0600

    agregan comentarios por partes

--- Analisis.R
+++ Analisis.R
@@ -1,3 +1,5 @@
+
+#Se cargan librerías y las bases de datos de las calificaciones 
 calificaciones_prepa <- read.csv("E:/Descargas/calificaciones_prepa.csv")
 install.packages("tidyverse")
 install.packages("rpart")
@@ -5,8 +7,19 @@ library(rpart)
 library(tidyverse)
 library(dplyr)
 library(ggplot)
+#se buscan valores nulos dentro de data frame 
+any(is.nan(calificaciones_prepa))
+mean(is.na(calificaciones_prepa))
+
+#se obtiene de la base de datos solo la calificacion de matematicas 1
 Mate_1<- select(calificaciones_prepa,Desc_Materia, sexo, Calif_Final) %>% filter(Desc_Materia=="MATEMATICAS_I")
+
+#Revisando los datos mediante tablas de frecuencia y graficas de frecuencia
+Mate_1 %>% count(sexo) %>% group_by(sexo)
+#se realiza grafica de barras de mateáticas 1 por sexo
 ggplot(calificaciones_prepa, aes(x=sexo))+geom_bar(fill="purple")
+
+#se obtiene media, desviacion estandar y mediana de matemáticas 1
 Media <-mean(Mate_1$Calif_Final)
 Desviacion <-sd(Mate_1$Calif_Final)
 Mediana <-median(Mate_1$Calif_Final) 
