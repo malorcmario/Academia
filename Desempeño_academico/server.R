6fb4eccbcd2fc560dffa0342ae4a89f93ec92fa2
malorcmario <malorcmario@gmail.com>
2024-02-17 11:39:04 -0600

    intentando shuny

--- Desempeño_academico/server.R
+++ Desempeño_academico/server.R
@@ -1,28 +1,37 @@
-#
-# This is the server logic of a Shiny web application. You can run the
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
+# Generación de un dashboard de tipo de selección dinámica
 
 library(shiny)
 
 # Define server logic required to draw a histogram
-function(input, output, session) {
-
-    output$distPlot <- renderPlot({
-
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-
-    })
-
-}
+shinyServer(function(input, output) {
+  
+  datasetImput <- reactive(
+    switch(input$dataset, 
+           "rock" = Media_L, 
+           "mtcars" = Media_M, 
+           "iris" = Media_LM)
+  )
+  
+  output$var <- renderUI({
+    
+    radioButtons("varname", 
+                 "elige una variable", 
+                 names(datasetImput()))
+  })
+  
+  output$plot <- renderPlot({
+    if(!is.null(input$varname)){
+      if(!input$varname %in% names(datasetImput())){
+        colname <- names(datasetImput())[1]
+        
+      } else {
+        colname <- input$varname
+      }
+      hist(datasetImput()[,colname],
+           main = paste("Histograma de", colname), 
+           xlab = colname)
+    }
+    
+  })
+  
+})
--- Desempeño_academico/ui.R
+++ Desempeño_academico/ui.R
@@ -1,33 +1,23 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
+# Generación de un dashboard de tipo de selección dinámica
 
 library(shiny)
 
 # Define UI for application that draws a histogram
-fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput("distPlot")
-        )
+shinyUI(fluidPage(
+  
+  # Application title
+  titlePanel("Elecciones dinámicas de Data Frames"),
+  
+  # Sidebar with a slider input for number of bins
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("dataset", "Selección del dataset", 
+                  c("mtcars", "rock", "iris")), 
+      uiOutput("var")
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(plotOutput("plot")
     )
-)
+  )
+))
